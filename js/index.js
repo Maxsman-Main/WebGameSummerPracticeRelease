4c8664c8c5779a29d3a4383011652882e72f426e
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-20 20:04:20 +1000

    map

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let canvas_game = document.getElementById("game");
 let context = canvas_game.getContext('2d');
 
-let loader = new ImageLoader(
+const loader = new ImageLoader(
     [
         {
             name: 'land',
@@ -10,6 +10,10 @@ let loader = new ImageLoader(
         {
             name: 'rock',
             src: "./images/rock.png"
+        },
+        {
+            name: 'hero',
+            src: "./images/hero.png"
         }
     ],
     (progress) => {
@@ -19,22 +23,28 @@ let loader = new ImageLoader(
         }
     }
 )
+const map = new Map();
 
 const SIZE_BLOCK = 100;
 
 function draw() {
     let land = loader.get('land');
     let rock = loader.get('rock');
-    for (let y = 0; y < map.length; ++y) {
-        for (let x = 0; x < map[y].length; ++x) {
+    let hero = loader.get('hero');
+
+    for (let y = 0; y < map.size_y(); ++y) {
+        for (let x = 0; x < map.size_x(); ++x) {
+
             let block_x = x * SIZE_BLOCK;
             let block_y = y * SIZE_BLOCK;
-            if (map[y][x] == 1) {
+            
+            if (map.get(x, y) == 1) {
                 context.drawImage(land, block_x, block_y, SIZE_BLOCK, SIZE_BLOCK);
                 context.drawImage(rock, block_x, block_y, SIZE_BLOCK, SIZE_BLOCK);
-            } else if (map[y][x] == 0) {
+            } else if (map.get(x, y)  == 0) {
                 context.drawImage(land, block_x, block_y, SIZE_BLOCK, SIZE_BLOCK);
             }
         }
     }
+    context.drawImage(hero, 0, 0, SIZE_BLOCK, SIZE_BLOCK);
 }
--- js/map.js
+++ js/map.js
@@ -1,7 +1,24 @@
-let map = [
-    [0, 1, 0, 0, 0],
-    [1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-]
+class Map {
+
+    constructor() {
+        this.data = [
+            [0, 1, 0, 0, 0],
+            [1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+    }
+
+    get(x, y) {
+        return this.data[y][x];
+    }
+
+    size_y() {
+        return this.data.length;
+    }
+
+    size_x() {
+        return this.data[0].length;
+    }
+}
