49ccf23ee75307ca9d27441672ee6f039abe21de
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-21 22:11:59 +1000

    ascii debug

--- js/debugRenderer.js
+++ js/debugRenderer.js
@@ -5,19 +5,23 @@ class DebugRenderer {
         this.player = player;
     }
 
-    show() {
+    show(div_dom) {
         console.log("DebugRender: show runned")
+        let table = document.createElement('table');
         for (let y = 0; y < this.map.sizeY; ++y) {
-            let row = `${y}: `;
+            let tr = document.createElement('tr');
             for (let x = 0; x < this.map.sizeX; ++x) {
+                let td = document.createElement('td');
                 if (player.coordinates.x == x && this.player.coordinates.y == y) {
-                    row += `${player.label} `;
+                    td.innerText = `${player.label}`;
                 } else {
-                    row += `${this.map.getCell(x, y).label} `;
+                    td.innerText = `${this.map.getCell(x, y).label}`;
                 }
+                tr.appendChild(td);
             }
-            console.log(row);
+            table.appendChild(tr);
         }
         console.log("DebugRender: show ended")
+        div_dom.innerHTML = table.outerHTML;
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 var player = new Player("Steve", '@', {x: 0, y: 0}, 4, []);
 var map = new Map(10, 10);
 var moveManager = new MoveManager(map, player);
-
 var renderer = new DebugRenderer(map, player);
+var debug_ascii_ui = document.getElementById("debug_ascii_ui");
 
-renderer.show();
+function update(event) {
+    let dx = 0;
+    let dy = 0;
+    switch (event.code) {
+        case 'KeyW':
+            dx = 0, dy = -1;
+            break;
+        case 'KeyA':
+            dx = -1, dy = 0;
+            break;
+        case 'KeyS':
+            dx = 0, dy = 1;
+            break;
+        case 'KeyD':
+            dx = 1, dy = 0;
+            break;
+        default:
+            break;
+    }
+    moveManager.move(player.coordinates.x + dx, player.coordinates.y + dy);
+    renderer.show(debug_ascii_ui);
+}
 
-moveManager.move(0, 1);
-moveManager.move(1, 1);
-moveManager.move(2, 3);
-
-document.addEventListener('keydown', (new KeyboardListener()).action);
+renderer.show(debug_ascii_ui);
+document.addEventListener('keydown', update);
