3dab28966d1a5b08629ecb07e7476050f038f9d9
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-21 22:53:12 +1000

    array out of bounds, not strict right bounds

--- js/moveManager.js
+++ js/moveManager.js
@@ -6,8 +6,8 @@ class MoveManager {
     }
 
     isCorrectCoordinates(x, y) {
-        return (0 <= x && x <= this.map.sizeX) &&
-               (0 <= y && y <= this.map.sizeY) &&
+        return (0 <= x && x < this.map.sizeX) &&
+               (0 <= y && y < this.map.sizeY) &&
                (Math.abs(x - this.player.coordinates.x) + Math.abs(y - this.player.coordinates.y) == 1);
     }
 
