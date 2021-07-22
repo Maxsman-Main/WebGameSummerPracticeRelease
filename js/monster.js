23b8fd4d4f987b1ec0595affacd0d67d1cda64e6
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-22 17:31:11 +1000

    additional methods for monster

--- js/monster.js
+++ js/monster.js
@@ -3,11 +3,34 @@ class Monster extends Creature {
     constructor(name, sprite, label, type, health, defense, attack, attackBooster) {
         super(name, sprite, label);
         this.type = type; 
-        this.health = health;
+        this.maxHealth = health;
+        this.currentHealth = health;
         this.defense = defense;
         this.attack = attack;
         this.attackBooster = attackBooster;
     }
+
+    attackEnemy(enemy){
+        enemy.currentHealth -= (this.attack - enemy.defense + this.attackBooster);
+    }
+
+    defenseHimself(){
+        this.defense += 5;
+    }
+
+
+    isDead() {
+        return this.currentHealth <= 0;
+    }
+
+    Heal() {
+        this.currentHealth = this.maxHealth;
+    }
+
+    getString() {
+        return `${this.name}, ${this.currentHealth}`;
+    }
+
 }
 
 class Chudila extends Monster {
