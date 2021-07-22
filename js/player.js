7b7674abc25dc933fef04f937709088d54b5f1ed
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-22 23:23:27 +1000

    player.js comments

--- js/player.js
+++ js/player.js
@@ -1,22 +1,34 @@
 class Player extends Creature {
 
+    /**
+     * @param {number} name 
+     * @param {image} sprite 
+     * @param {string} label some char, used for debugging
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} availableMoves 
+     * @param {[Creatures, ...]} availableCreatures 
+     */
     constructor(name, sprite, label, x, y, availableMoves, availableCreatures) {
         super(name, sprite, label);
-        // number
         this.x = x;
-        // number
         this.y = y;
-        // number
         this.availableMoves = availableMoves;
-        // [Creature, ...]
         this.availableCreatures = availableCreatures;
     }
 
+    /**
+     * @param {number} x
+     * @param {number} y 
+     */
     move(x, y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @returns {[number, number]}
+     */
     getCoordinates() {
         return [this.x, this.y];
     }
