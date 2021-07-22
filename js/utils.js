e6e831ca335cfa76fc2914a380af05651b877481
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-22 23:12:18 +1000

    utils.js comments

--- js/utils.js
+++ js/utils.js
@@ -1,13 +1,35 @@
 class Util {
 
+    /**
+     * Return a random number between a and b, inclusive
+     * 
+     * @param {number} a
+     * @param {number} b
+     * @returns {number}
+     */
     static random(a, b) {
         return Math.floor(Math.random() * (b - a + 1)) + a;
     }
 
+    /**
+     * Return a random object from an array 
+     * 
+     * @param {[Object, ...]} arr 
+     * @param {number} a 
+     * @param {number} b 
+     * @returns {Object}
+     */
     static randomItemFromArray(arr, a, b) {
         return arr[Util.random(0, arr.length - 1)];
     }
 
+    /**
+     * Return whether the number x is in the range 
+     * 
+     * @param {number} x
+     * @param {[number, number]} range 
+     * @returns {boolean}
+     */
     static isInRange(x, range) {
         return range[0] <= x && x <= range[1];
     }
