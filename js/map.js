2938c522059c3e2ee8e5a1303b218117c0f9a495
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-22 17:34:09 +1000

    new map generator with new cells

--- js/map.js
+++ js/map.js
@@ -11,15 +11,36 @@ class Map {
     }
 
     static generate(sizeX, sizeY) {
+        let defaultCell = LandCell;
+        let possibleCells = [
+            {
+                obj: MountCell,
+                rand: [5, 10]
+            }
+        ];
+        
         console.log(`Map: Generate, (${sizeX}, ${sizeY})`);
         var data = [];
         for (let y = 0; y < sizeY; ++y) {
             var row = [];
             for (let x = 0; x < sizeX; ++x) {
-                row.push(
-                    new Cell("\"", 'land', [10, 20], [
-                        new Chudila()
-                    ]));
+                
+                let randNum = Util.random(1, 100);
+                let objForCreate = null;
+                
+                for (let i = 0; i < possibleCells.length; ++i) {
+                    if (Util.isInRange(randNum, possibleCells[i].rand)) {
+                        objForCreate = possibleCells[i].obj;
+                        break;
+                    }
+                }
+
+                if (!objForCreate) {
+                    objForCreate = defaultCell;
+                }
+
+                row.push(new objForCreate());
+
             }
             data.push(row);
         }
--- js/utils.js
+++ js/utils.js
@@ -8,4 +8,8 @@ class Util {
         return arr[Util.random(0, arr.length - 1)];
     }
 
+    static isInRange(x, range) {
+        return range[0] <= x && x <= range[1];
+    }
+
 }
