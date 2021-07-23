155f1980f8923ef5725dd771699b0b96097f593c
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-23 13:55:26 +1000

    added cell.ts

---
+++ src/cell.ts
@@ -0,0 +1,34 @@
+import { Creature } from './creature';
+import { Utils } from './utils';
+
+class Cell {
+
+    private label: string;
+    private type: string;
+    private transtionCost: number;
+    private creature: Creature;
+
+    constructor(label: string, type: string,
+        transitionCostMinMax : [number, number], possibleCreatures: [Creature]) {
+        this.label = label;
+        this.type = type;
+        this.transtionCost = Utils.random.apply(this, transitionCostMinMax);
+        this.creature = Utils.randomItemFromArray(possibleCreatures);
+    }
+
+    public getLabel(): string {
+        return this.label;
+    }
+
+    public getType(): string {
+        return this.type;
+    }
+
+    public getTransitionCost(): number {
+        return this.transtionCost;
+    }
+
+    public getCreature(): Creature {
+        return this.creature;
+    }
+}
