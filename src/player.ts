ae4207046815c27d957dc95d5a9b85cf4728d6c6
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-23 16:19:02 +1000

    getters for player

--- src/player.ts
+++ src/player.ts
@@ -4,8 +4,16 @@ export class Player extends Creature {
 
     private x: number;
     private y: number;
-    private availableMoves: number;
-    private availableCreatures: Creature[];
+    private _availableMoves: number;
+    public get availableMoves(): number {
+        return this._availableMoves;
+    }
+
+    private _availableCreatures: Creature[];
+
+    public get availableCreatures(): Creature[] {
+        return this._availableCreatures;
+    }
 
     constructor(name: string, sprite : string, label: string,
         x: number, y: number, availableMoves: number,
@@ -13,8 +21,8 @@ export class Player extends Creature {
         super(name, sprite, label);
         this.x = x;
         this.y = y;
-        this.availableMoves = availableMoves;
-        this.availableCreatures = availableCreatures;
+        this._availableMoves = availableMoves;
+        this._availableCreatures = availableCreatures;
     }
 
     public move(x: number, y: number) {
