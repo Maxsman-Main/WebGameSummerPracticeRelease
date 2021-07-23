23985e4ba3f2efe249820dde5b145f46355dffed
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-23 13:55:09 +1000

    added utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+export class Utils {
+
+    /**
+     * @param a 
+     * @param b 
+     * @returns random number between a and b, inclusive
+     */
+    static random(a: number, b: number): number {
+        return Math.floor(Math.random() * (b - a + 1)) + a;
+    }
+
+    static randomItemFromArray(arr: [any]): any {
+        return arr[this.random(0, arr.length - 1)];
+    }
+}
