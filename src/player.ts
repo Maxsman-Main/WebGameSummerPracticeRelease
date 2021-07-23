e2e877ea6682e4f6c528eecaf054562aa759c42c
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-23 18:24:39 +1000

    addMonster, deleteMonster for player

--- src/player.ts
+++ src/player.ts
@@ -5,13 +5,13 @@ export class Player extends Creature {
 
     private x: number;
     private y: number;
+
     private _availableMoves: number;
     public get availableMoves(): number {
         return this._availableMoves;
     }
 
     private _availableMonsters: Monster[];
-
     public get availableCreatures(): Monster[] {
         return this._availableMonsters;
     }
@@ -33,6 +33,21 @@ export class Player extends Creature {
 
     public getCoordinates() : { x: number, y: number } {
         return { x: this.x, y: this.y };
+    }
+
+    public addMoster(monster: Monster) {
+        this._availableMonsters.push(monster);
+    }
+
+    public deleteMonster(monster: Monster) {
+        const index = this._availableMonsters.indexOf(monster);
+
+        const newArray = (index > -1) ? [
+            ...this._availableMonsters.slice(0, index),
+            ...this._availableMonsters.slice(index + 1)
+        ] : this._availableMonsters;
+
+        this._availableMonsters = newArray;
     }
 
 }
