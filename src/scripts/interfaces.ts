e13ccb0afa892e8500d022a75502cbb92141831a
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-26 21:02:56 +1000

    I2Dcoordinates

--- src/scripts/interfaces.ts
+++ src/scripts/interfaces.ts
@@ -3,7 +3,7 @@ export interface IHasCssClass {
 }
 
 export interface IHasCoordinates {
-    getCoordinates(): { x: number, y: number };
+    getCoordinates(): I2Dcoordinates;
 }
 
 export interface IDrawableInField extends IHasCoordinates, IHasCssClass {
@@ -13,4 +13,9 @@ export interface IDrawableInField extends IHasCoordinates, IHasCssClass {
 export interface ISceneInfo {
     name: string;
     element: HTMLElement;
+}
+
+export interface I2Dcoordinates {
+    x: number;
+    y: number;
 }
