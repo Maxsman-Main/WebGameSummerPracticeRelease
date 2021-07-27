ba6dd93857faec6145332e5fed60ffe7c1a0c50a
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-28 00:05:28 +1000

    it is enough to store only two monster fields

--- src/scripts/logic/fight.ts
+++ src/scripts/logic/fight.ts
@@ -1,17 +1,6 @@
 import {Monster} from "../creatures/monster"
 
 export class Fight {
-
-    private readonly _monsterFirst: Monster;
-    public get monsterFirst(): Monster {
-        return this._monsterFirst;
-    }
-
-    private readonly _monsterSecond: Monster;
-    public get monsterSecond(): Monster {
-        return this._monsterSecond;
-    }
-
     private _currentMonster: Monster;
     public get currentMonster(): Monster {
         return this._currentMonster;
@@ -22,26 +11,22 @@ export class Fight {
     }
 
     constructor(monsterFirst: Monster, monsterSecond: Monster) {
-        this._monsterFirst = monsterFirst;
-        this._monsterSecond = monsterSecond;
         this._currentMonster = monsterFirst;
         this._defenseMonster = monsterSecond;
     }
 
     public swap() {
-        [this._currentMonster, this._defenseMonster] =
-            [this.defenseMonster, this.currentMonster];
+        [this._currentMonster, this._defenseMonster] = [this.defenseMonster, this.currentMonster];
     }
 
     public isFinish(): boolean {
-        return this.monsterFirst.isDead() || this.monsterSecond.isDead();
+        return this.currentMonster.isDead() || this.defenseMonster.isDead();
     }
 
     public getWinner() {
         if (!this.isFinish) return null;
 
-        return (this.monsterFirst.isDead()) ? this.monsterSecond :
-            this.monsterFirst;
+        return (this.currentMonster.isDead()) ? this.defenseMonster : this.currentMonster;
     }
 
 }
