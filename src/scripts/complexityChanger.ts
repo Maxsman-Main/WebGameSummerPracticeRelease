826970eac5294a173eccec6379c9350590bb27de
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-29 18:27:50 +1000

    refactoring

--- src/scripts/complexityChanger.ts
+++ src/scripts/complexityChanger.ts
@@ -1,16 +1,15 @@
-import {Monster} from './creatures/monster'
-import {Cell,  LandCell, VolcanoCell, ForestCell, LakeCell, WhiteCastleCell, DarkCastleCell} from './map/cell';
+import {Cell} from './map/cell';
 
-export class ComplexityChanger{
+export class ComplexityChanger {
     private _currentMapData: Cell[][];
 
-    constructor(map: Cell[][]){
+    constructor(map: Cell[][]) {
         this._currentMapData = map;
     }
 
-    public balanceMap(): void{
-        for(let y = 0; y < this._currentMapData.length; ++y){
-            for(let x = 0; x < this._currentMapData[y].length; ++x){
+    public balanceMap(): void {
+        for (let y = 0; y < this._currentMapData.length; ++y) {
+            for (let x = 0; x < this._currentMapData[y].length; ++x) {
                 let currentMonster = this._currentMapData[y][x].monster;
                 let newHealthValue = (1 + x / 10) * currentMonster.health;
                 currentMonster.BalanceHeal(newHealthValue);
