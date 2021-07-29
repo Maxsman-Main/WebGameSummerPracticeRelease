c8a1c43f30ada8c9e042ecff51948f1a2727eaf4
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-30 00:21:24 +1000

    update

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore'
+import 'firebase/database'
 
 const firebaseConfig = {
     apiKey: "AIzaSyD3q-5oL3uBVysZM_rb486eG_FtespBNg4",
