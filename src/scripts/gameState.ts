3bb99e7d77453017cefc780565c3b277e3a19065
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-30 23:44:32 +1000

    game state store player2

--- src/scripts/gameState.ts
+++ src/scripts/gameState.ts
@@ -6,15 +6,16 @@ import {Fight} from "./logic/fight";
 
 export class GameState {
     public player: Player;
+    public player2: Player;
     public creatures: IDrawableInField[];
     public map: Map;
     public moveManager: MoveManager;
     public fight: Fight;
 
-    constructor(player: Player, map: Map, creatures: IDrawableInField[]) {
+    constructor(player: Player, player2: Player, map: Map) {
         this.player = player;
-        this.creatures = [player, ...creatures];
-        this.map = new Map(5, 5);
+        this.player2 = player2;
+        this.map = map;
         this.moveManager = new MoveManager(this.map, this.player);
         this.fight = null;
     }
