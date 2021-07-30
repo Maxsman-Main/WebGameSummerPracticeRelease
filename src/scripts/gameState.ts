3491d94cdd36b355d55060e1fa989222a4efda26
ada0l <andreika.varfolomeev@yandex.ru>
2021-07-30 23:17:38 +1000

    no need to store scenes in game state

--- src/scripts/gameState.ts
+++ src/scripts/gameState.ts
@@ -1,42 +1,21 @@
 import {Player} from './creatures/player';
 import {Map} from './map/map';
 import {MoveManager} from './logic/moveManager';
-import {IDrawableInField, ISceneInfo} from './interfaces';
+import {IDrawableInField} from './interfaces';
 import {Fight} from "./logic/fight";
 
 export class GameState {
-
     public player: Player;
     public creatures: IDrawableInField[];
     public map: Map;
     public moveManager: MoveManager;
-    public scenes: ISceneInfo[];
     public fight: Fight;
 
-    constructor(player: Player, creatures: IDrawableInField[]) {
+    constructor(player: Player, map: Map, creatures: IDrawableInField[]) {
         this.player = player;
-        this.creatures = creatures;
+        this.creatures = [player, ...creatures];
         this.map = new Map(5, 5);
         this.moveManager = new MoveManager(this.map, this.player);
-        this.scenes = [
-            {
-                name: 'start',
-                element: document.getElementById('game-start')
-            },
-            {
-                name: 'field',
-                element: document.getElementById('game-field')
-            },
-            {
-                name: 'fight',
-                element: document.getElementById('game-fight')
-            },
-            {
-                name: 'select-monster',
-                element: document.getElementById('game-select-monster')
-            }
-        ]
         this.fight = null;
     }
-
 }
